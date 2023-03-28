3adf6f09df1076d8fecebb6c84eb5cab8801617d
ikhsanmaaruf <126649363+ikhsanmaaruf@users.noreply.github.com>
2023-03-28 11:35:35 +0700

    v.1.4

--- proses.php
+++ proses.php
@@ -1,4 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="proses.php">
+        <input type="number" name="angka1"> <br>
+        <select name="operasi">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <br>
+        <input type="number" name="angka2"> <br>
+        <button type="submit">Hitung</button>
+    </form>
+    <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $angka1 = $_POST['angka1'];
         $angka2 = $_POST['angka2'];
@@ -18,3 +39,5 @@
         echo "Hasil: " . $hasil;
     }
 ?>
+</body>
+</html>
